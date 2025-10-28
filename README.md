e717f4a188a405f6d7cffd53aebed2acc634d4e9
jasonlau05 <jasonlaucr@gmail.com>
2025-10-28 11:12:06 -0500

    testing and schedule scripts

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 using robin_stocks to automate trades.
+run daily scripts with windows task scheduler.
 
 pls do not day trade
--- dailyDCA.py
+++ dailyDCA.py
@@ -1,14 +1,16 @@
 import datetime
 from robin import BUY, QUOTE
 from areMarketsOpen import is_market_open
+import os, sys
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 print('today is', datetime.date.today())
 
 # run this script every morning at market open
 
 def main():
-    if is_market_open:
-    BUY('VOO', 5)
+    if is_market_open():
+        BUY('VOO', 5)
 
 if __name__ == "__main__":
     main()
